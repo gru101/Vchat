9ecb0cb2911c4e8f893e96761e38dc0ea4eaac50
gru101 <grukandgal@gmail.com>
2025-06-08 17:35:33 +0530

    Fixed asgi.py

--- Vchat/asgi.py
+++ Vchat/asgi.py
@@ -6,10 +6,11 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
+from django.core.asgi import get_asgi_application
+application = get_asgi_application()
 
 import os
 import django
-from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import re_path, path
 from channels.auth import AuthMiddlewareStack
@@ -19,7 +20,6 @@ from Vchat.routing import websocket_urlpatterns
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Vchat.settings')
 django.setup()
 
-application = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": application,
