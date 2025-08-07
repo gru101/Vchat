1345fcbd3db6c63735d3ae27feacc8b6d4ba209b
gru101 <grukandgal@gmail.com>
2025-08-07 22:58:26 +0530

    changed django.setup order

--- Vchat/asgi.py
+++ Vchat/asgi.py
@@ -6,17 +6,22 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
-from django.core.asgi import get_asgi_application
+
+
+
 import os
 import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Vchat.settings')
+django.setup()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import re_path, path
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 from Vchat.routing import websocket_urlpatterns
+from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Vchat.settings')
-django.setup()
 
 django_asgi_app = get_asgi_application()
 
