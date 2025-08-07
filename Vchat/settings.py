d619fe95fc3d706377a5c95f685f7eaca975dd84
gru101 <grukandgal@gmail.com>
2025-08-07 23:03:08 +0530

    updated allowed hosts

--- Vchat/settings.py
+++ Vchat/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # DEBUG = os.getenv("DEBUG") == "True"
 DEBUG = False
 
-ALLOWED_HOSTS = ['vchat-rq33.onrender.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['vchat-rq33.onrender.com', '127.0.0.1', 'localhost','vchat-69cw.onrender.com']
 CSRF_TRUSTED_ORIGINS = ['https://vchat-rq33.onrender.com']
 # Application definition
 
